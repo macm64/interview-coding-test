f63f71227127b95bdb94ca903a87b2333ef8bbad
root <root@MiguelC>
2018-07-20 15:35:07 -0400

    Made a change to test a single type of product

--- spec/coTestSpec.js
+++ spec/coTestSpec.js
@@ -7,9 +7,17 @@ const Product = coTest.Product;
 describe("Co Test", function() {
 
   it("should foo", function() {
-    const coTest = new CarInsurance([ new Product("foo", 0, 0) ]);
-    const products = coTest.updatePrice();
-    expect(products[0].name).equal("fixme");
+    const coTest = new CarInsurance([ new Product('Super Sale', 3, 6) ]);
+    const productPrinter = function (product) {
+      console.log(`${product.name}, ${product.sellIn}, ${product.price}`);
+    };
+    for (let i = 1; i <= 30; i += 1) {
+      console.log(`Day ${i}`);
+      console.log('name, sellIn, price');
+      coTest.updatePrice().forEach(productPrinter);
+      console.log('');
+    }
+    expect(coTest.products[0].price).equal(0);
   });
 
 });
